7b457194e721f8a722b9f2cc76b2af802a70f77c
Diego Dall'Agnol <diegodagnol@outlook.com>
2026-06-06 10:15:11 -0300

    fix: usa HashRouter no admin para evitar conflito de basename

--- hipermidia.admin/src/App.jsx
+++ hipermidia.admin/src/App.jsx
@@ -1,31 +1,35 @@
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { AdminContext, AdminUI, Resource } from 'react-admin';
+import { HashRouter } from 'react-router-dom';
 import authProvider from './authProvider';
 import dataProvider from './dataProvider';
 import { LocalList, LocalCreate, LocalEdit } from './resources/locais';
 import { UsuarioList, UsuarioShow, UsuarioEdit } from './resources/usuarios';
 
 const App = () => (
-  <Admin
-    title="Explocus Admin"
-    authProvider={authProvider}
-    dataProvider={dataProvider}
-    basename="/admin"
-  >
-    <Resource
-      name="locais"
-      list={LocalList}
-      create={LocalCreate}
-      edit={LocalEdit}
-      options={{ label: 'Locais' }}
-    />
-    <Resource
-      name="usuarios"
-      list={UsuarioList}
-      show={UsuarioShow}
-      edit={UsuarioEdit}
-      options={{ label: 'Usuários' }}
-    />
-  </Admin>
+  <HashRouter>
+    <AdminContext
+      title="Explocus Admin"
+      authProvider={authProvider}
+      dataProvider={dataProvider}
+    >
+      <AdminUI>
+        <Resource
+          name="locais"
+          list={LocalList}
+          create={LocalCreate}
+          edit={LocalEdit}
+          options={{ label: 'Locais' }}
+        />
+        <Resource
+          name="usuarios"
+          list={UsuarioList}
+          show={UsuarioShow}
+          edit={UsuarioEdit}
+          options={{ label: 'Usuários' }}
+        />
+      </AdminUI>
+    </AdminContext>
+  </HashRouter>
 );
 
 export default App;
